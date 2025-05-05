b7c0161a55d9e35e8d9667cc21543597dead2006
jsp6b05 <jsp6b05@scs.local>
2025-05-05 17:32:47 +0800

    auth_branch

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginController;
+
 Route::get('/', function () {
  return view('welcome');
 });
 Route::resource('products', ProductController::class);
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
